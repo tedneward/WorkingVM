da889087252c362e619f3941a623b2191055d37b
Ted Neward <ted@tedneward.com>
2022-05-31 03:45:58 -0700

    Testing multiple Crystal spec files

---
+++ crystal/spec/stack_spec.cr
@@ -0,0 +1,14 @@
+require "./spec_helper"
+
+describe SimpleVM do
+  # TODO: Write tests
+
+  it "works" do
+    false.should eq(false)
+  end
+
+  it "doesnt work" do
+    # Uncomment to test if broken tests do anything
+    #false.should eq(true)
+  end
+end
