1772dc04b13009914bee3d43a1b091fe30b17626
Ted Neward <ted@tedneward.com>
2022-06-15 19:22:58 -0700

    First pass at Swift implementation

--- swift/README.md
+++ swift/README.md
@@ -1,3 +1,9 @@
 # swift
 
 A description of this package.
+
+## CLI
+Build: `swift build` from the this directory (the same one in which you find this README).
+
+Test: `swift test` from the this directory
+
--- swift/Sources/simplevm/simplevm.swift
+++ swift/Sources/simplevm/simplevm.swift
@@ -1,3 +1,53 @@
 struct simplevm {
-    var text = "Hello, World!"
+    var text = "SimpleVM"
+    var version = "0.0"
+}
+
+public enum Bytecode {
+    case NOP
+    case DUMP
+    case TRACE
+}
+
+public class VirtualMachine {
+
+    // Tracing
+    //
+    var trace = false
+    func trace(_ message: String...) {
+        if trace {
+            print("TRACE: \(message.joined())")
+        }
+    }
+
+    // Diagnostic dump
+    //
+    func dump() {
+        print("SimpleVM v0.0")
+        print("=============")
+    }
+
+    var sp = -1
+    var stack = [Int](repeating: 0, count: 100)
+    var Stack : [Int] {
+        get { return Array(stack[0 ..< (sp + 1)]) }
+    }
+    public func push(_ value: Int) { 
+        trace("  ----> pushed \(value)")
+        sp += 1
+        stack[sp] = value
+    }
+    public func pop() -> Int { 
+        let ret = stack[sp]
+        sp -= 1
+        trace("  ----> popped \(ret)")
+        return ret
+    }
+
+    public func execute(_ opcode: Bytecode, _ operands: Int...) {
+
+    }
+    public func execute(_ code: [Bytecode]) {
+
+    }
 }
